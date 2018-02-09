f94df52ff8fefab49f1355f271667a45a22b0a61
dragospopa <dragos.popa21@yahoo.com>
2018-02-09 10:00:39 +0000

    fixed search.js issue

--- _plugins/algolia_hooks.rb
+++ _plugins/algolia_hooks.rb
@@ -3,6 +3,7 @@ module Jekyll
     module Hooks
       def self.should_be_excluded?(filepath)
         return true if filepath =~ %r{/shells/toolbelt.md}
+		return true if filepath =~ %r{/styleguide}
         false
       end
     end
