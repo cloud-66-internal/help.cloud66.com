159c8aa2a5cabd7e65bd0b47f22c54cf06e90a04
Vic van Gool <vic@cloud66.com>
2020-11-24 10:51:56 +0000

    make it wait

--- tools/buildkite-redirects-generator.sh
+++ tools/buildkite-redirects-generator.sh
@@ -14,6 +14,7 @@ mkdir -p netlify-redirects
 tools/redirects-generator.rb --input="$pwd/netlify.toml" --output="$pwd/netlify-redirects/netlify-redirects.html"
 # start a webserver for this
 nohup npx http-server "$pwd/netlify-redirects" -a 127.0.0.1 >/tmp/http-server.log 2>&1 &
+read varname
 # run the link checker
 blc "http://127.0.0.1:8080/netlify-redirects.html" -r -o -f --host-request 2 --requests 2 -g --filter-level 3 -e
 # all done
