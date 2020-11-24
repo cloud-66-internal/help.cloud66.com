802adb080cf84a770cc2e9985827881fd2c0edb7
Vic van Gool <vic@cloud66.com>
2020-11-24 10:54:42 +0000

    add more log output

--- tools/buildkite-redirects-generator.sh
+++ tools/buildkite-redirects-generator.sh
@@ -10,12 +10,15 @@ fi
 pwd=$(pwd)
 echo " ---> Generating netlify-redirects.html via tools/redirects-generator.rb"
 # run the site generator
+echo "mkdir -p netlify-redirects"
 mkdir -p netlify-redirects
+echo "tools/redirects-generator.rb --input=$pwd/netlify.toml --output=$pwd/netlify-redirects/netlify-redirects.html"
 tools/redirects-generator.rb --input="$pwd/netlify.toml" --output="$pwd/netlify-redirects/netlify-redirects.html"
 # start a webserver for this
+echo "nohup npx http-server $pwd/netlify-redirects -a 127.0.0.1 >/tmp/http-server.log 2>&1 &"
 nohup npx http-server "$pwd/netlify-redirects" -a 127.0.0.1 >/tmp/http-server.log 2>&1 &
-read varname
 # run the link checker
+echo "blc http://127.0.0.1:8080/netlify-redirects.html -r -o -f --host-request 2 --requests 2 -g --filter-level 3 -e"
 blc "http://127.0.0.1:8080/netlify-redirects.html" -r -o -f --host-request 2 --requests 2 -g --filter-level 3 -e
 # all done
 echo "---> I'm Mr. Meeseeks! Look at me!"
